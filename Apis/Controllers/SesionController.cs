baec7a741bd081917dc0806878c93bf99d9569ca
N3TWORKL3G3ND <rojascastrojavier13@gmail.com>
2025-05-22 23:27:46 -0600

    Nueva API para probar la conexion

--- Apis/Controllers/SesionController.cs
+++ Apis/Controllers/SesionController.cs
@@ -4,6 +4,8 @@ using Contracts.Requests;
 using Contracts.Responses;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace Apis.Controllers
 {
@@ -13,10 +15,29 @@ namespace Apis.Controllers
     public class SesionController : ControllerBase
     {
         private readonly ISesionService _sesionService;
+        private readonly IConfiguration _configuration;
 
-        public SesionController(ISesionService sesionService)
+        public SesionController(ISesionService sesionService, IConfiguration configuration)
         {
             _sesionService = sesionService;
+            _configuration = configuration;
+        }
+
+
+
+        [HttpGet("test-db")]
+        public IActionResult TestDbConnection()
+        {
+            try
+            {
+                using var conn = new SqlConnection(_configuration.GetConnectionString("GCloudConnection"));
+                conn.Open();
+                return Ok("¡Conexión exitosa!");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
         }
 
 
